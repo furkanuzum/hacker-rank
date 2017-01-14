49cb1831e196c71e0006b173e607a19b3745b743
furkan <furkanuzum61@gmail.com>
2017-01-14 18:06:54 +0200

    initial hacker rank commit

--- src/introduction/s3/Solution.java
+++ src/introduction/s3/Solution.java
@@ -1,6 +1,13 @@
 package introduction.s3;
 
+import java.util.Scanner;
+
 /**
  * https://www.hackerrank.com/challenges/java-if-else?h_r=next-challenge&h_v=zen
  */
+public class Solution {
 
+    public static void main(String[] args) {
+
+    }
+}
