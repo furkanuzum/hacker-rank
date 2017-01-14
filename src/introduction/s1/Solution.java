6a87e777615575e514060bee8cd487a0649f12da
furkan <furkanuzum61@gmail.com>
2017-01-14 18:00:20 +0200

    initial hacker rank commit

---
+++ src/introduction/s1/Solution.java
@@ -0,0 +1,11 @@
+package introduction.s1;
+
+//https://www.hackerrank.com/challenges/welcome-to-java
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named introduction.s1.Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
---
+++ src/introduction/s3/Solution.java
@@ -0,0 +1,6 @@
+package introduction.s3;
+
+/**
+ * https://www.hackerrank.com/challenges/java-if-else?h_r=next-challenge&h_v=zen
+ */
+
